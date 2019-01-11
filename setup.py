a4cc76df4bc6b5f7356b771ef0c2a36e06d7c90c
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2019-01-11 14:04:54 +0100

    Fixed setup.py
    Change-Id: I06e9b2176d96e175683a4b74f0a7809cc653dda8

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
 """Space synthesis"""
-# pylint: disable=R0801
-import os
-from setuptools import setup
-from setuptools import find_packages
+
+import imp
+from setuptools import setup, find_packages
 
 
-VERSION = "0.0.0"
+VERSION = imp.load_source("region_grower.version", "region_grower/version.py").VERSION
 
 
-setup(**{
-    'version': VERSION,
-    'install_requires': [
+setup(
+    name='region-grower',
+    version=VERSION,
+    install_requires=[
         'voxcell>=2.5.4',
         'click>=7.0',
         'tqdm>=4.28.1',
         'tns>=1.0.1',
     ],
-    'packages': find_packages(),
-    'extras_require': {
-        'validate': ['mayavi==4.5.0'],
+    packages=find_packages(),
+    extras_require={
+        'validate': [
+            'mayavi==4.5.0',
+        ],
     },
-    'entry_points': {
-        'console_scripts': ['region-grower=region_grower.app:cli']},
-    'name': 'region_grower'
-})
+    entry_points={
+        'console_scripts': [
+            'region-grower=region_grower.app:cli'
+        ]
+    },
+)
