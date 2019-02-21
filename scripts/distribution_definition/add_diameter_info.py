694107a0536f89087884616d5b0d139971e0a1c1
Benoît Coste <benoit.coste@epfl.ch>
2019-02-21 15:19:29 +0100

    Update script to incorporate diameter parameters into distribs
    Change-Id: Ida09df793542a637eb946728e180324ed98cb4c4

--- scripts/distribution_definition/add_diameter_info.py
+++ scripts/distribution_definition/add_diameter_info.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+import os
 import argparse
 import json
 
@@ -5,15 +7,28 @@ def main(args):
     with open(args.input_distrib) as f:
         d = json.load(f)
 
-    with open(args.diameter_info) as f:
-        diam = json.load(f)
+    mtypes_input_distrib = set(d['mtypes'])
+    mtypes_from_diameter_folder = set()
 
+    for f in os.listdir(args.diameter_folder):
+        if not f.endswith('.json'):
+            continue
+        mtypes_from_diameter_folder.add(f[:-5])
+
+    missing_from_input_distrib = mtypes_from_diameter_folder - mtypes_input_distrib
+    if missing_from_input_distrib:
+        print('Mtypes missing from keys of {}: {}'.format(args.input_distrib, missing_from_input_distrib))
+
+    missing_from_diameter_folder = mtypes_input_distrib - mtypes_from_diameter_folder
+    if missing_from_diameter_folder:
+        print('Mtypes missing from diameter folder: {}'.format(missing_from_diameter_folder))
 
     for mtype in d['mtypes'].keys():
-        d['mtypes'][mtype]['diameter'] = diam
+        with open(os.path.join(args.diameter_folder, '{}.json'.format(mtype))) as f:
+            d['mtypes'][mtype]['diameter'] = json.load(f)
 
     with open(args.output_distrib, 'w+') as f:
-        json.dump(d, f)
+        json.dump(d, f, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
@@ -23,8 +38,8 @@ if __name__ == '__main__':
         help="Input distributions"
     )
     parser.add_argument(
-        "diameter_info",
-        help="Diameter info",
+        "diameter_folder",
+        help="Diameter folder",
     )
     parser.add_argument(
         "output_distrib",
