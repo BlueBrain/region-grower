d5ca1b9e96e8911ccb9a6bb3c14c427a22ec1e7c
arnaudon <alexis.arnaudon@epfl.ch>
2020-10-07 15:07:19 +0200

    Use brainbuilder in atlas_mock for tests
    Change-Id: Ib00ad2a696ee52357e469e90ecbfef49a4c92f54

--- tests/test_atlas_helper.py
+++ tests/test_atlas_helper.py
@@ -1,7 +1,7 @@
+import numpy as np
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-import numpy as np
 from nose.tools import assert_equal
 from nose.tools import assert_raises
 
@@ -10,42 +10,37 @@ from region_grower.atlas_helper import AtlasHelper
 from voxcell.nexus.voxelbrain import Atlas
 from voxcell.exceptions import VoxcellError
 
-from region_grower import RegionGrowerError
 
-DATA = Path(__file__).parent / 'data'
+DATA = Path(__file__).parent / "data"
 
 
 def test_atlas_helper():
-    '''All test are made in a single function as we do not want to regenerate
+    """All test are made in a single function as we do not want to regenerate
     the atlas for each test
-    '''
+    """
     with TemporaryDirectory() as tempdir:
         small_O1(tempdir)
         helper = AtlasHelper(Atlas.open(tempdir))
         assert_equal(helper.layer_thickness(1).raw[5, 5, 5], 200)
 
-        assert_equal(helper.pia_coord.raw[5, 5, 5], 1000)
+        assert_equal(helper.pia_coord.raw[5, 5, 5], 200)
 
         cortical_depths = np.cumsum([4] * 6)
 
         # test in L1
-        assert_equal(helper.lookup_target_reference_depths([1000, 200, 1000], cortical_depths),
-                     (200, 4))
+        assert_equal(
+            helper.lookup_target_reference_depths([100, -100, 100], cortical_depths),
+            (300, 8),
+        )
 
-        assert_equal(helper.lookup_target_reference_depths([1000, 212.6, 1000], cortical_depths),
-                     (200, 4))
+        assert_equal(
+            helper.lookup_target_reference_depths([100, 0, 100], cortical_depths),
+            (200, 4),
+        )
 
-        # test in Lr
-        assert_equal(helper.lookup_target_reference_depths([1000, 1000, 1000], cortical_depths),
-                     (700, 20))
-
-        # cell in L6
-        assert_equal(helper.lookup_target_reference_depths([1000, 1200, 1000], cortical_depths),
-                     (1000, 24))
-
-
-        assert_raises(VoxcellError, helper.lookup_target_reference_depths,
-                      [10000, 0, 0], cortical_depths)
-
-        assert_raises(RegionGrowerError, helper.lookup_target_reference_depths,
-                      [1000, 1400, 1000], cortical_depths)
+        assert_raises(
+            VoxcellError,
+            helper.lookup_target_reference_depths,
+            [10000, 0, 0],
+            cortical_depths,
+        )
--- tests/test_context.py
+++ tests/test_context.py
@@ -3,13 +3,18 @@ from tempfile import TemporaryDirectory
 
 import numpy as np
 from nose.tools import assert_equal
+from nose.tools import assert_almost_equal
+from nose.tools import assert_raises
 from numpy.testing import assert_array_almost_equal
 
-from atlas_mock import small_O1
-from region_grower.context import SpaceContext
 from voxcell.nexus.voxelbrain import Atlas
 
-DATA = Path(__file__).parent / 'data'
+from atlas_mock import small_O1
+from region_grower import RegionGrowerError
+from region_grower.context import SpaceContext
+
+
+DATA = Path(__file__).parent / "data"
 
 
 def test_context():
@@ -17,36 +22,53 @@ def test_context():
 
     with TemporaryDirectory() as tempdir:
         small_O1(tempdir)
-        context = SpaceContext(Atlas.open(tempdir),
-                               DATA / 'distributions.json',
-                               DATA / 'parameters.json')
+        context = SpaceContext(
+            Atlas.open(tempdir), DATA / "distributions.json", DATA / "parameters.json"
+        )
 
-        result = context.synthesize([1000, 1000, 1000], 'L2_TPC:A')
+    result = context.synthesize([100, -100, 100], "L2_TPC:A")
 
-    assert_array_almost_equal(result.apical_points, np.array([[ 0.505352, 67.794989, -1.691661]]))
+    assert_array_almost_equal(
+        result.apical_points, np.array([[9.40834, 114.985021, -25.603346]])
+    )
 
     # This tests that input orientations are not mutated by the synthesize() call
-    assert_array_almost_equal(context.tmd_parameters['L2_TPC:A']['apical']['orientation'],
-                              [[0.0, 1.0, 0.0]])
+    assert_array_almost_equal(
+        context.tmd_parameters["L2_TPC:A"]["apical"]["orientation"], [[0.0, 1.0, 0.0]]
+    )
 
-    assert_array_almost_equal(result.neuron.soma.points,
-                              np.array([[-9.1213598 ,  0.34562492, 0.],
-                                        [ 4.6709661 , -7.8600206 , 0.],
-                                        [ 9.1049757 , -0.83503044, 0.],
-                                        [ 0.12655652,  9.0572395 , 0.],
-                                        [ 0.        ,  9.1431866 , 0.],
-                                        [-4.1508913 ,  7.8628845 , 0.],
-                                        [-9.0374002 ,  1.3868117 , 0.]],
-                                       dtype=np.float32))
+    assert_array_almost_equal(
+        result.neuron.soma.points,
+        np.array(
+            [
+                [-9.1213598, 0.34562492, 0.0],
+                [4.6709661, -7.8600206, 0.0],
+                [9.1049757, -0.83503044, 0.0],
+                [0.12655652, 9.0572395, 0.0],
+                [0.0, 9.1431866, 0.0],
+                [-4.1508913, 7.8628845, 0.0],
+                [-9.0374002, 1.3868117, 0.0],
+            ],
+            dtype=np.float32,
+        ),
+    )
 
     assert_equal(len(result.neuron.root_sections), 7)
-    assert_array_almost_equal(next(result.neuron.iter()).points,
-                              np.array([[ 0.126557,  9.05724 ,  1.244288],
-                                        [ 0.13983 , 10.007194,  1.374793],
-                                        [ 0.249028, 10.898591,  1.370834]],
-                                       dtype=np.float32))
-    assert_array_almost_equal(next(result.neuron.iter()).diameters,
-                              np.array([ 0.6, 0.6, 0.6], dtype=np.float32))
+    assert_array_almost_equal(
+        next(result.neuron.iter()).points,
+        np.array(
+            [
+                [0.126557, 9.05724, 1.244288],
+                [0.13983, 10.007194, 1.374793],
+                [0.249028, 10.898591, 1.370834],
+            ],
+            dtype=np.float32,
+        ),
+    )
+    assert_array_almost_equal(
+        next(result.neuron.iter()).diameters,
+        np.array([0.6, 0.6, 0.6], dtype=np.float32),
+    )
 
 
 def test_context_external_diametrizer():
@@ -54,36 +76,110 @@ def test_context_external_diametrizer():
 
     with TemporaryDirectory() as tempdir:
         small_O1(tempdir)
-        context = SpaceContext(Atlas.open(tempdir),
-                               DATA / 'distributions_external_diametrizer.json',
-                               DATA / 'parameters_external_diametrizer.json')
+        context = SpaceContext(
+            Atlas.open(tempdir),
+            DATA / "distributions_external_diametrizer.json",
+            DATA / "parameters_external_diametrizer.json",
+        )
 
-        result = context.synthesize([1000, 1000, 1000], 'L2_TPC:A')
+    result = context.synthesize([100, -100, 100], "L2_TPC:A")
 
-    assert_array_almost_equal(result.apical_points, np.array([[ 0.505352, 67.794989, -1.691661]]))
+    assert_array_almost_equal(
+        result.apical_points, np.array([[9.40834, 114.985021, -25.603346]])
+    )
 
     # This tests that input orientations are not mutated by the synthesize() call
-    assert_array_almost_equal(context.tmd_parameters['L2_TPC:A']['apical']['orientation'],
-                              [[0.0, 1.0, 0.0]])
+    assert_array_almost_equal(
+        context.tmd_parameters["L2_TPC:A"]["apical"]["orientation"], [[0.0, 1.0, 0.0]]
+    )
 
-    assert_array_almost_equal(result.neuron.soma.points,
-                              np.array([[-9.1213598 ,  0.34562492, 0.],
-                                        [ 4.6709661 , -7.8600206 , 0.],
-                                        [ 9.1049757 , -0.83503044, 0.],
-                                        [ 0.12655652,  9.0572395 , 0.],
-                                        [ 0.        ,  9.1431866 , 0.],
-                                        [-4.1508913 ,  7.8628845 , 0.],
-                                        [-9.0374002 ,  1.3868117 , 0.]],
-                                       dtype=np.float32))
-
+    assert_array_almost_equal(
+        result.neuron.soma.points,
+        np.array(
+            [
+                [-9.1213598, 0.34562492, 0.0],
+                [4.6709661, -7.8600206, 0.0],
+                [9.1049757, -0.83503044, 0.0],
+                [0.12655652, 9.0572395, 0.0],
+                [0.0, 9.1431866, 0.0],
+                [-4.1508913, 7.8628845, 0.0],
+                [-9.0374002, 1.3868117, 0.0],
+            ],
+            dtype=np.float32,
+        ),
+    )
 
     assert_equal(len(result.neuron.root_sections), 7)
-    assert_array_almost_equal(next(result.neuron.iter()).points,
-                              np.array([[ 0.126557,  9.05724 ,  1.244288],
-                                        [ 0.13983 , 10.007194,  1.374793],
-                                        [ 0.249028, 10.898591,  1.370834]],
-                                       dtype=np.float32))
+    assert_array_almost_equal(
+        next(result.neuron.iter()).points,
+        np.array(
+            [
+                [0.126557, 9.05724, 1.244288],
+                [0.13983, 10.007194, 1.374793],
+                [0.249028, 10.898591, 1.370834],
+            ],
+            dtype=np.float32,
+        ),
+    )
 
-    assert_array_almost_equal(next(result.neuron.iter()).diameters,
-                              np.array([0.807079, 0.806512, 0.8059810],
-                                       dtype=np.float32))
+    assert_array_almost_equal(
+        next(result.neuron.iter()).diameters,
+        np.array([0.983045, 0.981993, 0.981009], dtype=np.float32),
+    )
+
+
+def test_verify():
+
+    with TemporaryDirectory() as tempdir:
+        small_O1(tempdir)
+        context = SpaceContext(
+            Atlas.open(tempdir), DATA / "distributions.json", DATA / "parameters.json"
+        )
+
+    context.verify(["L2_TPC:A"])
+
+    assert_raises(RegionGrowerError, context.verify, ["UNKNOWN_MTYPE"])
+
+    del context.tmd_parameters["L2_TPC:A"]
+
+    assert_raises(RegionGrowerError, context.verify, ["L2_TPC:A"])
+
+
+def test_scale():
+    np.random.seed(0)
+
+    with TemporaryDirectory() as tempdir:
+        small_O1(tempdir)
+        context = SpaceContext(
+            Atlas.open(tempdir), DATA / "distributions.json", DATA / "parameters.json"
+        )
+
+    result = context.synthesize([100, -100, 100], "L2_TPC:A")
+
+    assert_array_almost_equal(
+        result.apical_points, np.array([[9.40834, 114.985021, -25.603346]])
+    )
+
+    # Test scale computation
+    neurite = result.neuron.root_sections[0]
+
+    params = context.tmd_parameters["L2_TPC:A"]
+
+    # Set bias value
+    params["apical"]["bias_length"] = 1
+
+    assert params.get("apical", {}).get("modify", {}) is None
+    assert params.get("basal", {}).get("modify", {}) is None
+
+    fixed_params = context._correct_position_orientation_scaling(
+        params, [100, -100, 100]
+    )
+
+    expected = {"reference_thickness": 314, "target_thickness": 300.0}
+    assert (
+        fixed_params.get("apical", {}).get("modify", {}).get("kwargs", {}) == expected
+    )
+    assert_almost_equal(
+        fixed_params.get("apical", {}).get("bias_length"), 0.9554140127388535
+    )
+    assert fixed_params.get("basal", {}).get("modify", {}).get("kwargs", {}) == expected
