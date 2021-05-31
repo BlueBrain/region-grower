Fix computation of max_ph in scale_default_barcode
Change-Id: Id72e9a091f891d2005b996aae33168f15dd54ae4

region_grower/modify.py
@@ -27,11 +27,10 @@ def scale_default_barcode(
     Reference thickness defines the property of input data.
     Target thickness defines the property of space, which should be used by synthesis.
     """
-    max_ph = np.max(persistent_homologies)
-    scaling_reference = np.nan_to_num(reference_thickness / max_ph, nan=np.inf)
+    max_ph = np.nanmax([i[0] for i in persistent_homologies])
     # If cell is larger than the reference thickness it should be scaled down
     # This ensures that the cell will not grow more than the target thickness
-    scaling_reference = min(scaling_reference, 1.0)
+    scaling_reference = min(reference_thickness / max_ph, 1.0)
 
     scaling_ratio = scaling_reference * target_thickness / reference_thickness
 

tests/test_context.py
@@ -97,10 +97,10 @@ class TestSpaceWorker:
         # Synthesize in L2
         result = context_worker.synthesize()
 
-        assert_array_equal(result.apical_sections, np.array([98]))
+        assert_array_equal(result.apical_sections, np.array([97]))
         assert_array_almost_equal(
             result.apical_points,
-            np.array([[19.62138748, 353.8174438, 12.29593754]]),
+            np.array([[-7.19463158, 266.21151733, 12.50336075]]),
         )
 
         # This tests that input orientations are not mutated by the synthesize() call
@@ -169,10 +169,10 @@ class TestSpaceWorker:
 
         result = context_worker.synthesize()
 
-        assert_array_equal(result.apical_sections, np.array([98]))
+        assert_array_equal(result.apical_sections, np.array([97]))
         assert_array_almost_equal(
             result.apical_points,
-            np.array([[19.62138748, 353.8174438, 12.29593754]]),
+            np.array([[-7.19463158, 266.21151733, 12.50336075]]),
         )
 
         # This tests that input orientations are not mutated by the synthesize() call
@@ -216,14 +216,7 @@ class TestSpaceWorker:
         assert_array_almost_equal(
             next(result.neuron.iter()).diameters,
             np.array(
-                [
-                    0.8767099380493164,
-                    0.8752812147140503,
-                    0.8737207651138306,
-                    0.8710576295852661,
-                    0.868639349937439,
-                    0.8664852380752563,
-                ],
+                [0.774056, 0.773625, 0.773154, 0.772351, 0.771622, 0.770973],
                 dtype=np.float32,
             ),
         )
@@ -435,15 +428,15 @@ class TestSpaceWorker:
                     },
                     "scaling": [
                         {
-                            "max_ph": np.nan,
+                            "max_ph": 126.96580088057513,
                             "scaling_ratio": 0.9554140127388535,
                         },
                         {
-                            "max_ph": np.nan,
+                            "max_ph": 139.93140451880743,
                             "scaling_ratio": 0.9554140127388535,
                         },
                         {
-                            "max_ph": np.nan,
+                            "max_ph": 143.28779114733433,
                             "scaling_ratio": 0.9554140127388535,
                         },
                     ],

tests/test_modify.py
@@ -22,7 +22,7 @@ def test_scale_default_barcode():
     res = modify.scale_default_barcode(
         np.array(ph) * 10, None, reference_thickness, target_thickness
     )
-    assert_array_equal(res, [[0.25, 0.5], [0.75, 1.0]])
+    assert_array_equal(res, [[1 / 3, 2 / 3], [1, 4 / 3]])
 
 
 def test_scale_target_barcode():
@@ -118,19 +118,21 @@ class TestOutputScaling:
         assert modify.output_scaling(root_sec, [0, 1, 0], 1, None) == 1
 
     def test_output_scaling_min(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], 9.76777, None) == pytest.approx(1.2)
+        assert modify.output_scaling(root_sec, [0, 1, 0], 39.789311, None) == pytest.approx(1.2)
 
     def test_output_scaling_useless_max(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], None, 10) == 1
+        assert modify.output_scaling(root_sec, [0, 1, 0], None, 1000) == 1
 
     def test_output_scaling_max(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], None, 6.51185) == pytest.approx(0.8)
+        assert modify.output_scaling(root_sec, [0, 1, 0], None, 26.5262075) == pytest.approx(0.8)
 
     def test_output_scaling_useless_min_useless_max(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], 1, 10) == 1
+        assert modify.output_scaling(root_sec, [0, 1, 0], 1, 1000) == 1
 
     def test_output_scaling_useless_min_max(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], 1, 6.51185) == pytest.approx(0.8)
+        assert modify.output_scaling(root_sec, [0, 1, 0], 1, 26.5262075) == pytest.approx(0.8)
 
     def test_output_scaling_min_max(self, root_sec):
-        assert modify.output_scaling(root_sec, [0, 1, 0], 9.76777, 6.51185) == pytest.approx(1.2)
+        assert modify.output_scaling(root_sec, [0, 1, 0], 39.789311, 26.5262075) == pytest.approx(
+            1.2
+        )