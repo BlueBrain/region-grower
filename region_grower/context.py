4b97315a3d62fffd61fd7e1cbeaa8fc26c04e82d
Adrien Berchet <adrien.berchet@epfl.ch>
2024-11-07 17:38:43 +0100

    Chore: Fix URLs and metadata and add license headers (#5)

--- region_grower/context.py
+++ region_grower/context.py
@@ -7,6 +7,16 @@ TLDR: SpaceContext.synthesized() is being called by
 the placement_algorithm package to synthesize circuit morphologies.
 """
 
+# LICENSE HEADER MANAGED BY add-license-header
+#
+# Copyright (c) 2023-2024 Blue Brain Project, EPFL.
+#
+# This file is part of region-grower.
+# See https://github.com/BlueBrain/region-grower for further info.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 import os
 from collections import defaultdict
 from copy import deepcopy
